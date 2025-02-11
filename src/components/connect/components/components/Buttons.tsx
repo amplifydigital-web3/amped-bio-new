75df2f246010c0c2b3682e564b47f5ce5342f1fd
Gustavo <gustavo@obex.pw>
2025-02-11 15:36:10 -0300

    removed old buttons

--- src/components/connect/components/components/Buttons.tsx
+++ src/components/connect/components/components/Buttons.tsx
@@ -43,61 +43,6 @@ export const BaseButton = styled.button<{
     `}
 `<any>
 
-export const CTAButton = styled(BaseButton)<any>`
-  background: var(--on-fire);
-  color: #fff;
-  width: fit-content;
-  border: none;
-
-  div {
-    color: #fff;
-  }
-
-  &:hover {
-    // opacity: 0.85;
-    // scale: 1.01;
-    color: #fff;
-  }
-
-  &:disabled {
-    opacity: 0.7;
-    filter: grayscale(0.5);
-    cursor: not-allowed;
-    &:hover {
-      scale: 1;
-    }
-  }
-`
-
-export const ProgrammeButton = styled(BaseButton)`
-  width: 100%;
-  justify-content: flex-start;
-  align-items: center;
-  font-weight: 500;
-  padding-right: 8px;
-  padding-left: 8px;
-  background: var(--button-widget);
-  color: #fff;
-  border: none;
-  border-radius: 8px;
-
-  div {
-    padding-left: 8px;
-    display: flex;
-    justify-content: space-between;
-    flex: 1;
-    letter-spacing: unset;
-    font-size: 0.8rem;
-    color: #fff;
-  }
-
-  &:hover {
-    background: var(--button-widget);
-    filter: brightness(0.95);
-    border: none;
-  }
-`
-
 export const BrandedProgrammeButton = styled(BaseButton)<{
   background?: string
 }>`
@@ -133,23 +78,5 @@ export const BrandedProgrammeButton = styled(BaseButton)<{
     opacity: 0.7;
     filter: grayscale(0.9);
     cursor: not-allowed;
-  }
-`
-
-export const SocialLoginButton = styled(ProgrammeButton)`
-  border: 2px solid var(--widget-contrast);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  div {
-    color: var(--widget-contrast);
-    flex: unset;
-    padding-right: 12px;
-    padding-left: 0;
-  }
-
-  &:hover {
-    border: 2px solid var(--widget-contrast);
   }
 `
