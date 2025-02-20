bb9262e0264177a2d7287da51370f816c0437f97
Will-Frey <will@npaymelabs.com>
2025-02-20 18:44:06 -0500

    more vercel testing

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
-import adminRoutes from './routes/admin.routes';
 
 dotenv.config();
 
@@ -16,19 +13,14 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Express on Vercel'));
 app.get('/testing', (req, res) => res.send('Express on Vercel test'));
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/admin', adminRoutes);
-
 // Error handling middleware
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
