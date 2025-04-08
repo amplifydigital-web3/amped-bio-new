2b72e42b74bb5da3c570220aa31eb00a749586b0
Gustavo <gustavo@obex.pw>
2025-04-08 16:19:49 -0300

    removed unused env

--- server/src/env.ts
+++ server/src/env.ts
@@ -9,16 +9,6 @@ export const env = cleanEnv(process.env, {
     desc: 'Port for the server to listen on',
     default: 3000,
   }),
-  SMTP2GO_API_KEY: str({
-    desc: 'API key for SMTP2GO email service',
-    default: '',
-    example: 'api-XXXXXXXXXXXXXXXXXXXX',
-  }),
-  SMTP2GO_EMAIL: str({
-    desc: 'Email address used for sending emails via SMTP2GO',
-    default: 'noreply@example.com',
-    example: 'noreply@yourdomain.com',
-  }),
   VERCEL_PROJECT_PRODUCTION_URL: url({
     desc: 'URL for the site in production',
     default: 'http://localhost:5173',
