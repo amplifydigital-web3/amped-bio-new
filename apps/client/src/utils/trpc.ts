96c8d1a7db67632c8c6344c14ad40f5b522eef7a
Gustavo <gustavo@obex.pw>
2025-05-05 15:35:00 -0300

    use related project with trpc

--- apps/client/src/utils/trpc.ts
+++ apps/client/src/utils/trpc.ts
@@ -1,10 +1,16 @@
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../../../server/src/trpc";
+import { withRelatedProject } from "@vercel/related-projects";
+
+const baseURL = withRelatedProject({
+  projectName: "amped-bio-server",
+  defaultHost: "http://localhost:43000",
+});
 
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: `${import.meta.env.VITE_API_URL || "http://localhost:43000"}/trpc`,
+      url: `${baseURL}/trpc`,
     }),
   ],
 });
