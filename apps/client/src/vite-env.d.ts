ee9ab9c46468b00e78c0816b96f9e9303bb46b65
Gustavo <gustavo@obex.pw>
2025-05-06 13:56:35 -0300

    fix VITE_API_URL

--- apps/client/src/vite-env.d.ts
+++ apps/client/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_VERCEL_ENV?: string;
+  readonly VITE_REWARD_URL: string;
+  readonly VITE_PUBLIC_WALLETCONNECT_PROJECT_ID: string;
+  readonly VITE_SHOW_REWARD: string;
+  readonly VITE_SHOW_CREATOR_POOL: string;
+  readonly VITE_SHOW_RNS: string;
+  readonly VITE_SHOW_GALLERY: string;
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
