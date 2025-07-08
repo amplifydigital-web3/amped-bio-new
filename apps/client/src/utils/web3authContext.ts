ec7b7f6b026eae2461f3c609a04a93b0761362a4
Gustavo <gustavo@obex.pw>
2025-07-08 18:43:19 -0300

    update env

--- apps/client/src/utils/web3authContext.ts
+++ apps/client/src/utils/web3authContext.ts
@@ -1,8 +1,7 @@
 import { WEB3AUTH_NETWORK } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 
-const clientId =
-  "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw"; // get from https://dashboard.web3auth.io
+const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
 
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
