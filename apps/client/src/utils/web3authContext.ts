5bf87cebb9cf8815079c63468cf805b3df36771e
Gustavo <gustavo@obex.pw>
2025-07-10 10:40:36 -0300

    add log and default chain id

--- apps/client/src/utils/web3authContext.ts
+++ apps/client/src/utils/web3authContext.ts
@@ -3,8 +3,11 @@ import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 
 const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
 
+console.info("Web3Auth Client ID:", clientId);
+
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
+    defaultChainId: "0x12085", // revochain testnet chain ID
     clientId,
     enableLogging: true,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
