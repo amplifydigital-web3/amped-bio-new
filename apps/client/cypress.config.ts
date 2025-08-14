6bbc09e5b49aab4c6fd7997663510e101f6d9091
Gustavo <gustavo@obex.pw>
2025-08-14 11:51:49 -0300

    fix cypress

--- apps/client/cypress.config.ts
+++ apps/client/cypress.config.ts
@@ -1,14 +1,13 @@
-// @ts-expect-error - CJS module not supported
-import { defineConfig } from "cypress";
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+const { defineConfig } = require("cypress");
 
-// @ts-expect-error - CJS module not supported
+// @ts-ignore
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     supportFile: false,
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
   },
 });
