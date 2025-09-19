3eb946e9c3ad8e63c37bc325fae90c65b1aec84d
Gustavo <gustavo@obex.pw>
2025-09-19 12:14:06 -0300

    fix web3auth network

--- apps/client/src/utils/web3authContext.ts
+++ apps/client/src/utils/web3authContext.ts
@@ -1,7 +1,8 @@
-import { WEB3AUTH_NETWORK, type Web3AuthOptions } from "@web3auth/modal";
+import { type WEB3AUTH_NETWORK_TYPE, type Web3AuthOptions } from "@web3auth/modal";
 
 const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
 const defaultChainId = import.meta.env.VITE_DEFAULT_NETWORK_ID_HEX;
+const web3AuthNetwork = import.meta.env.VITE_WEB3AUTH_NETWORK as WEB3AUTH_NETWORK_TYPE;
 
 console.info("Web3Auth Client ID:", clientId);
 console.info("Web3Auth Default Chain ID:", defaultChainId);
@@ -10,7 +11,7 @@ const web3AuthOptions: Web3AuthOptions = {
   defaultChainId,
   clientId,
   enableLogging: false,
-  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  web3AuthNetwork: web3AuthNetwork,
 };
 
 const web3AuthContextConfig = {
