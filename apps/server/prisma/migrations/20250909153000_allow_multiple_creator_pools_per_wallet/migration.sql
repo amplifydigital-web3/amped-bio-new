64f482c8df9f04f8e71c30f981d05b8971ca64bd
Gustavo <gustavo@obex.pw>
2025-11-04 11:03:10 -0300

    created the migration

--- apps/server/prisma/migrations/20250909153000_allow_multiple_creator_pools_per_wallet/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `creator_pools` ADD COLUMN `chainId` VARCHAR(191) NOT NULL DEFAULT '1';
-
--- CreateIndex
-CREATE UNIQUE INDEX `creator_pools_userId_chainId_key` ON `creator_pools`(`userId`, `chainId`);
-
--- DropIndex
-DROP INDEX `creator_pools_userId_key` ON `creator_pools`;
