164d7397f6b3353c9bc6e5e4f3ea6d2da6475413
Gustavo <gustavo@obex.pw>
2025-11-18 10:14:37 -0300

    fix migration

--- apps/server/prisma/migrations/20251113023445_change_bigint_fields_to_text_for_large_numbers/migration.sql
+++ apps/server/prisma/migrations/20251113023445_change_bigint_fields_to_text_for_large_numbers/migration.sql
@@ -1,8 +1,8 @@
 -- AlterTable
-ALTER TABLE `creator_pools` MODIFY `revoStaked` TEXT NOT NULL DEFAULT '0';
+ALTER TABLE `creator_pools` MODIFY `revoStaked` TEXT NOT NULL;
 
 -- AlterTable
 ALTER TABLE `stake_events` MODIFY `amount` TEXT NOT NULL;
 
 -- AlterTable
-ALTER TABLE `staked_pools` MODIFY `stakeAmount` TEXT NOT NULL DEFAULT '0';
+ALTER TABLE `staked_pools` MODIFY `stakeAmount` TEXT NOT NULL;
