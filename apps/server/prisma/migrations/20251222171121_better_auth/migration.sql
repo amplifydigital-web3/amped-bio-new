fe74f5547f597248952d58ffc95846ff65cfab55
Gustavo <gustavo@obex.pw>
2025-12-22 14:15:06 -0300

    feat: Revamp authentication models and database schema, removing obsolete fields and adding email verification

---
+++ apps/server/prisma/migrations/20251222171121_better_auth/migration.sql
@@ -0,0 +1,78 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `auth_as` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `email_verified_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `provider` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `provider_id` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `remember_token` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the `refresh_tokens` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `refresh_tokens` DROP FOREIGN KEY `refresh_tokens_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `users` DROP COLUMN `auth_as`,
+    DROP COLUMN `email_verified_at`,
+    DROP COLUMN `password`,
+    DROP COLUMN `provider`,
+    DROP COLUMN `provider_id`,
+    DROP COLUMN `remember_token`,
+    ADD COLUMN `email_verified` BOOLEAN NOT NULL DEFAULT false;
+
+-- DropTable
+DROP TABLE `refresh_tokens`;
+
+-- CreateTable
+CREATE TABLE `session` (
+    `id` VARCHAR(191) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `ipAddress` VARCHAR(191) NULL,
+    `userAgent` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `session_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `account` (
+    `id` VARCHAR(191) NOT NULL,
+    `accountId` VARCHAR(191) NOT NULL,
+    `providerId` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `accessToken` VARCHAR(191) NULL,
+    `refreshToken` VARCHAR(191) NULL,
+    `idToken` VARCHAR(191) NULL,
+    `accessTokenExpiresAt` DATETIME(3) NULL,
+    `refreshTokenExpiresAt` DATETIME(3) NULL,
+    `scope` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `verification` (
+    `id` VARCHAR(191) NOT NULL,
+    `identifier` VARCHAR(191) NOT NULL,
+    `value` LONGTEXT NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NULL,
+    `updatedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `session` ADD CONSTRAINT `session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `account` ADD CONSTRAINT `account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
