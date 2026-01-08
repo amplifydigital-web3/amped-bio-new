38c9b9a4b9f496c5feadaf08e93c94d3ef8d2c33
Gustavo <gustavo@obex.pw>
2026-01-07 23:15:12 -0300

    feat: enable server to listen on configured port
    Configure server to listen on the port specified in environment variables by adding app.listen() with the PORT from env configuration. This change activates the server's ability to accept incoming connections.

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,8 @@
 import app from "./services/API";
+import { env } from "./env";
 import "./bootstrap";
 // the line below is necessary to make sure vercel recognizes this file as an edge function
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-imports
 import express from "express";
 
-export default app;
+app.listen(env.PORT, () => console.log(`listening on port ${env.PORT}`));
